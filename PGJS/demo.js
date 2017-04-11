77026b11712b5b5a7b7a5fffba7eac3a4263bfa9
Nitin Choudhary <ernitinjai@users.noreply.github.com>
2017-04-11 17:39:52 +0530

    Update demo.js

--- PGJS/demo.js
+++ PGJS/demo.js
@@ -1,26 +1,20 @@
-//var dom = require('./miniDom');
-//var adsSetupPlugin = require('./ads-setup-plugin');
-//var messages = require('./messages');
-
-//videojs.plugin('ads-setup', adsSetupPlugin);
-
-dom.onReady(function() {
-  
-  videojs.plugin('ads-setup', function (opts) {
-    var player = this;
-    var adsCancelTimeout = 3000;
-
-    var vastAd = player.vastClient({
-      //Media tag URL
-      adTagUrl: "http://pubads.g.doubleclick.net/gampad/ads?env=....",
-      playAdAlways: true,
-      //Note: As requested we set the preroll timeout at the same place than the adsCancelTimeout
-      adCancelTimeout: adsCancelTimeout,
-      adsEnabled: !!options.adsEnabled
-    });
-});
-
-   
-
-    
-});
+<video id="example_video_1" class="video-js vjs-default-skin"
+    controls preload="auto" width="640" height="264"
+    poster="http://vjs.zencdn.net/v/oceans.png"
+    data-setup='{
+      "plugins": {
+      "vastClient": {
+        "adTagUrl": "http://pubads.g.doubleclick.net/gampad/ads?env=....",
+        "adCancelTimeout": 5000,
+        "adsEnabled": true
+        }
+      }
+    }'>
+  <source src="http://vjs.zencdn.net/v/oceans.mp4" type='video/mp4'/>
+  <source src="http://vjs.zencdn.net/v/oceans.webm" type='video/webm'/>
+  <source src="http://vjs.zencdn.net/v/oceans.ogv" type='video/ogg'/>
+  <p class="vjs-no-js">
+    To view this video please enable JavaScript, and consider upgrading to a web browser that
+    <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+  </p>
+</video>
