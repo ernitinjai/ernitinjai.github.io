37c3928b7cc369789b8a83e0f50746f73d15c60e
Nitin <nitinc@personagraph.com>
2017-04-17 10:28:35 +0530

    added contract class mydemo

---
+++ PG/mydemo.js
@@ -0,0 +1,32 @@
+function initForm(tagObj) {
+
+    
+
+    var width = tagObj.width; 
+    var height = tagObj.height;
+    var tagUrl = tagObj.tagUrl;   
+    //var tagEl = document.getElementById('input.tag-el');
+    var videoContainer = document.getElementById('vjs-video-container');
+    var videoElement = document.getElementById('video-js');
+
+    videoElement.style.width = width;
+    
+
+    var adPluginOpts = {
+              "adCancelTimeout":20000,// Wait for ten seconds before canceling the ad.
+              "adsEnabled": true,
+              //"adTagXML":this.adResp
+              "url":tagUrl
+        };
+          //adPluginOpts.plugins["ads-setup"].adTagXML = XML
+      this.player = videojs(videoElement, adPluginOpts, function() {
+       });
+
+
+      var vastAd = this.player.vastClient(adPluginOpts);
+
+      this.player.play();
+    
+    
+
+  }
