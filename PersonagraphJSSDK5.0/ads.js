ad36a04225d8ddbecd235604ebb10ea752580ffc
ankuradhikari08 <ankuradhikari08@gmail.com>
2017-06-16 12:31:39 +0530

    Add files via upload

---
+++ PersonagraphJSSDK5.0/ads.js
@@ -0,0 +1,117 @@
+var Ads = function() {
+
+   this.player = videojs('content_video');
+   this.adResp = '';
+ // Remove controls from the player on iPad to stop native controls from stealing
+  // our click
+  var contentPlayer =  document.getElementById('content_video_html5_api');
+  if ((navigator.userAgent.match(/iPad/i) ||
+          navigator.userAgent.match(/Android/i)) &&
+      contentPlayer.hasAttribute('controls')) {
+    contentPlayer.removeAttribute('controls');
+  }
+
+  // Start ads when the video player is clicked, but only the first time it's
+  // clicked.
+  var startEvent = 'click';
+  if (navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/Android/i)) {
+    startEvent = 'touchend';
+  }
+  this.player.one(startEvent, this.bind(this, this.init));
+
+  this.options = {
+    id: 'content_video'
+  };
+
+  this.events = [
+    pgads.AdEvent.Type.ALL_ADS_COMPLETED,
+    pgads.AdEvent.Type.CLICK,
+    pgads.AdEvent.Type.COMPLETE,
+    pgads.AdEvent.Type.FIRST_QUARTILE,
+    pgads.AdEvent.Type.LOADED,
+    pgads.AdEvent.Type.MIDPOINT,
+    pgads.AdEvent.Type.PAUSED,
+    pgads.AdEvent.Type.STARTED,
+    pgads.AdEvent.Type.THIRD_QUARTILE,
+    pgads.AdEvent.Type.STOPPED,
+    pgads.AdEvent.Type.FULLSCREEN,
+    pgads.AdEvent.Type.LOAD_META_DATA
+    
+  ];
+
+  this.console = document.getElementById('pg-sample-console');
+  this.player.pgads(
+      this.options,
+      this.bind(this, this.adsManagerLoadedCallback));
+   
+};
+
+Ads.prototype.adsManagerLoadedCallback = function() {
+  for (var index = 0; index < this.events.length; index++) {
+    this.player.pgads.addEventListener(
+        this.events[index],
+        this.bind(this, this.onAdEvent));
+  }
+  var a = this.player.pgads.startFromReadyCallback();
+};
+
+Ads.prototype.onAdEvent = function(event) {
+ 
+  if (navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i)) {
+      document.location.href = 'vpaidpgevent://'+ event.type;
+      //alert('ios');
+  } else if(navigator.userAgent.match(/Android/i)) {
+     Android.showToast(''+ event.type);
+     //alert('android');
+  }
+  this.log('Ad event: ' + event.type);
+};
+
+Ads.prototype.executeVastXML = function(vastResp,width,height) {
+
+  
+ var adResp = atob(vastResp);
+  if (vastResp == '' && vastResp.length > 0) {
+      this.onAdEvent('VAST_EMPTY_RESPONSE_ERROR');
+      this.log('Error: please fill in an ad tag');
+  } else {
+    this.player.pgads.initializeAdDisplayContainer();
+    this.player.pgads.setContentWithAdsResponse(null, adResp, true); 
+    this.player.pgads.requestAds();
+     var elem = document.getElementsByClassName('video-js')[0];
+    elem.style.width = width;
+    elem.style.height = height;
+  }
+};
+
+Ads.prototype.executeVastTagUrl = function(tagUrl,width,height) {
+  
+  if (this.tagUrl == ''  && tagUrl.length > 0) {
+      this.onAdEvent('VAST_INVALID_TAG_ERROR');
+      this.log('Error: please fill in an ad tag');
+  } else {
+    this.player.pgads.initializeAdDisplayContainer();
+    this.player.pgads.setContentWithAdTag(null, tagUrl, true); 
+    this.player.pgads.requestAds();
+    var elem = document.getElementsByClassName('video-js')[0];
+    elem.style.width = width;
+    elem.style.height = height;
+  }
+};
+
+
+Ads.prototype.log = function(message) {
+  //this.console.innerHTML = this.console.innerHTML + '<br/>' + message;
+};
+
+Ads.prototype.bind = function(thisObj, fn) {
+  return function() {
+    fn.apply(thisObj, arguments);
+  };
+};
+
+
+
